ec5a9bc96e1a41f39db689e934265d3df65ae61f
Satu Ylikoski <satu.ylikoski@freska.fi>
2020-03-29 23:44:08 +0300

    Update the project to meet the requirements in 2020 edition

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Full stack open 2018 - Open course from University of Helsinki's Department of Computer Science.
+## Fullstack open 2020 - Open course from University of Helsinki's Department of Computer Science.
+
+This is the backend side of the course
+
+### Course parts
+
+- [x] Phone numbers: Exercises 3.1-3.6
+
+### Frontend
+
+- From the repository `full-stack-open` you will find frontend side for this project
--- index.js
+++ index.js
@@ -1,87 +1,97 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
-const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
+// This is needed in order to get into the request data
+app.use(express.json());
+
+// Custom token for request body
+morgan.token("body", function(req, res) {
+  return JSON.stringify(req.body);
+});
+
+// Logging the requests
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
+// Remove all the CORS issues for now with this
+app.use(cors());
 
 const PORT = 3001;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
+// Initial person data
 let persons = [
   {
     name: "Rasmus Virtanen",
     number: "044 123 4567",
-    date: '2017-12-10T17:30:31.098Z',
+    date: "2017-12-10T17:30:31.098Z",
     id: 1
   },
   {
     name: "Satu Koivisto",
     number: "050 333 4567",
-    date: '2017-12-10T18:39:34.091Z',
+    date: "2017-12-10T18:39:34.091Z",
     id: 2
   },
   {
     name: "Kaarle Majamaa",
     number: "040 221 1234",
-    date: '2017-12-10T19:20:14.298Z',
+    date: "2017-12-10T19:20:14.298Z",
     id: 3
   }
 ];
 
 const generateId = () => {
-  const maxId = persons.length > 0
-  ? persons.map(p => p.id).sort((a,b) => a - b).reverse()[0]
-  : 0;
+  const maxId =
+    persons.length > 0
+      ? persons
+          .map(p => p.id)
+          .sort((a, b) => a - b)
+          .reverse()[0]
+      : 0;
 
   return maxId + 1;
-}
+};
 
-app.get('/api/persons', (req, res) => {
+app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
-app.get('/info', (req, res) => {
+app.get("/info", (req, res) => {
   const sum = persons.length;
-
   res.send(`
-    <h2>There is ${sum} phonenumbers in total</h2>
+    <h2>Phonebook has info for ${sum} people</h2>
     <br/>
     <h4>Date of request: ${new Date()}</h4>
-  `)
+  `);
 });
 
-app.get('/api/persons/:id', (req, res) => {
+app.get("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
   const person = persons.find(p => p.id === id);
 
   person ? res.json(person) : res.status(404).end();
 });
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter(p => p.id !== id);
-
-  res.status(204).end();
-});
-
-app.post('/api/persons', (req, res) => {
+app.post("/api/persons", (req, res) => {
   const body = req.body;
 
   if (body.name === undefined) {
-    return res.status(400).json({ error: 'Name is missing'});
+    return res.status(400).json({ error: "name must be unique" });
   } else if (body.number === undefined) {
-    return res.status(400).json({ error: 'Number is missing'})
+    return res.status(400).json({ error: "number must be unique" });
   } else {
     const nameCheck = persons.find(p => p.name === body.name);
-    console.log('nameCheck', nameCheck);
+
     if (nameCheck) {
-      return res.status(400).json({ error: "Name already exists" });
+      return res.status(400).json({ error: "name already exists" });
     }
   }
-
 
   const person = {
     name: body.name,
@@ -95,5 +105,15 @@ app.post('/api/persons', (req, res) => {
   res.json(person);
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(p => p.id !== id);
 
+  res.status(204).end();
+});
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
